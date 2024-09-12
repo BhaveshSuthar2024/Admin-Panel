b09cec49df069efc30886cece1642e5d9145dff1
BhaveshSuthar2024 <bsuthar2024@gmail.com>
2024-09-12 18:29:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,253 @@
+document.getElementById("dashboard_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="block";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("dashboard_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("table_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("form_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("card_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("charts_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("modal_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("element_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("contacts_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("maps_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("checklist_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+document.getElementById("setting_option").addEventListener("click", function() { 
+    if (window.innerWidth <= 650) {
+        document.getElementById("side_container").style.display="none";
+    }
+    else {
+        document.getElementById("side_container").style.display="block"; 
+    }
+});
+
+
+
+document.getElementById("table_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="block";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("form_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="block";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("card_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="block";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("charts_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="block";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("modal_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="block";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("element_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="block";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("contacts_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="block";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("maps_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="block";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("checklist_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="block";
+    document.getElementById("setting").style.display="none";
+});
+document.getElementById("setting_option").addEventListener("click", function() {
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("table").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("card").style.display="none";
+    document.getElementById("charts").style.display="none";
+    document.getElementById("modal").style.display="none";
+    document.getElementById("element").style.display="none";
+    document.getElementById("contacts").style.display="none";
+    document.getElementById("maps").style.display="none";
+    document.getElementById("checklist").style.display="none";
+    document.getElementById("setting").style.display="block";
+});
+document.getElementById("bars").addEventListener("click", function() {
+    document.getElementById("side_container").style.display = "block";
+    document.getElementById("profile_container").style.display = "none";
+    document.getElementById("side_container").style.position = "absolute";
+    document.getElementById("side_container").style.marginTop = "75.31px";
+    document.getElementById("side_container").style.backgroundColor = "rgb(177, 177, 177)";   
+    document.getElementById("side_container").style.height = "calc(100vh - 78.31px)";
+});
+
